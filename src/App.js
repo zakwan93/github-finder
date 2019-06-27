db8b20103086b05a6c1d83ae6fca80dc3ed744a8
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-06-27 16:53:00 -0400

    worked on search button which retrive search user's github profile using Github API and also set Props at Up level from low level

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 import axios from "axios";
 import "./App.css";
 
@@ -10,23 +11,36 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
 
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?clint_id=${
+  //       process.env.REACT_APP_GIHUB_CLIENT_ID
+  //     }&client_secret=${process.env.REACT_APP_GIHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  //   // console.log(res.data);
+  // }
+
+  // Search Github User
+  searchUsers = async text => {
+    // console.log(text);
+    this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users?clint_id=${
+      `https://api.github.com/search/users?q=${text}&clint_id=${
         process.env.REACT_APP_GIHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GIHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data, loading: false });
-    console.log(res.data);
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
