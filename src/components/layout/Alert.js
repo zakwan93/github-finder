0b22347d46a52b060bea5a025f4d3bac59cc3d49
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-06-28 19:36:20 -0400

    chnage the whole app from classbased to functional base and use context hook for manage state and props

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,7 +1,10 @@
-import React from "react";
-// import PropTypes from "prop-types";
+import React, { useContext } from "react";
+import AlertContext from "./../../context/alert/alertContext";
 
-const Alert = ({ alert }) => {
+const Alert = () => {
+  const alertContext = useContext(AlertContext);
+  const { alert } = alertContext;
+
   return (
     alert != null && (
       <div className={`alert alert-${alert.type}`}>
@@ -11,7 +14,5 @@ const Alert = ({ alert }) => {
     )
   );
 };
-
-// Alert.propTypes = {};
 
 export default Alert;
