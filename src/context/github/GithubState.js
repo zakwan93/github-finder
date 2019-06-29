f4a0d03e1b2fc6697d0a6aa1ab733c9795eca62f
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-06-28 22:27:50 -0400

    deployed on netlify

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -11,6 +11,17 @@ import {
   GET_REPOS
 } from "../types";
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== "production") {
+  githubClientId = process.env.REACT_APP_GIHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GIHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GIHUB_CLIENT_ID;
+  githubClientSecret = process.env.GIHUB_CLIENT_SECRET;
+}
+
 const GithubState = props => {
   const initialState = {
     users: [],
@@ -25,9 +36,7 @@ const GithubState = props => {
   const searchUsers = async text => {
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${
-        process.env.REACT_APP_GIHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GIHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
@@ -40,9 +49,7 @@ const GithubState = props => {
   const getUser = async username => {
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${
-        process.env.REACT_APP_GIHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GIHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
@@ -56,9 +63,7 @@ const GithubState = props => {
     setLoading(true);
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=6&sort=created:asc&client_id=${
-        process.env.REACT_APP_GIHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GIHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}/repos?per_page=6&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
